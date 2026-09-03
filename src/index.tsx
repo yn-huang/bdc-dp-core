ce056fe36a64dcebfba8c80b8d95bbaaca33a9cf
Yinuo Huang <yinuo_huang@med.unc.edu>
2026-09-03 12:52:41 -0400

    change webpack to cra, to align with existing bdc dp environment

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement,
+);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
